67e41c2ff4a61cfedd233378d61d1dd8a072f112
Alex Rogna <alexrogna@yahoo.com>
2020-12-18 17:07:34 +0000

    add validation for email to match regexp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true 
   validates :email, length: {in: 5..50}
+  validates :email, format: {with: /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i}
   
   has_one :profile
   has_many :articles, ->{order 'published_at DESC, title ASC'},
