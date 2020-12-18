af713782515fe0f82d2f402774d6593def951fda
Alex Rogna <alexrogna@yahoo.com>
2020-12-18 15:45:34 +0000

    Declared rich many-to-many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :articles, ->{order 'published_at DESC, title ASC'},
     #dependent: :destroy #if you want to delete all articles belong to user when the user is deleted
     dependent: :nullify
+  has_many :replies, through: :articles, source: :comments
 end
