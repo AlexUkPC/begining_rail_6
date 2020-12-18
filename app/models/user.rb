3db83ac55b155c577cf1e9f8b6d755cfe4a9877a
Alex Rogna <alexrogna@yahoo.com>
2020-12-18 15:02:48 +0000

    Specified dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_one :profile
-  has_many :articles, ->{order 'published_at DESC, title ASC'}
+  has_many :articles, ->{order 'published_at DESC, title ASC'},
+    #dependent: :destroy #if you want to delete all articles belong to user when the user is deleted
+    dependent: :nullify
 end
