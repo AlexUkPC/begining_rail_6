4930087d47e0fe1024719aafac3ea6ef1eb97bc4
AlexUkPC <alexrogna@yahoo.com>
2021-01-15 18:13:35 +0000

    attachments added

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -8,6 +8,11 @@ class NotifierMailer < ApplicationMailer
   def email_friend(article, sender_name, receiver_email)
     @article = article
     @sender_name = sender_name
+
+    if @article.cover_image.present?
+      attachments[@article.cover_image.filename.to_s] = @article.cover_image.download
+    end
+
     mail to: receiver_email, subject: 'Interesting Article' 
   end
 end
