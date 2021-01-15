3d5a1fd23eb7ab74a51f1274ef309804fe903bbf
AlexUkPC <alexrogna@yahoo.com>
2021-01-15 19:17:35 +0000

    processign the emails

--- app/mailboxes/draft_articles_mailbox.rb
+++ app/mailboxes/draft_articles_mailbox.rb
@@ -1,4 +1,19 @@
 class DraftArticlesMailbox < ApplicationMailbox
   def process
+    author.articles.create(
+      title: mail.subject,
+      body: mail.body,
+    )
+  end
+  private
+  def require_author
+    bounce_with DraftArticlesMailbox.no_author(mail.from) unless author
+  end
+  def author
+    @author ||=User.find_by(draft_article_token: token)
+  end
+  
+  def token
+    mail.to.first.split('@').first
   end
 end
---
+++ app/mailers/draft_articles_mailer.rb
@@ -0,0 +1,15 @@
+class DraftArticlesMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.draft_articles_mailer.no_author.subject
+  #
+  def no_author
+    mail to: to, subject: "Your email could not be processed" do |format|
+      content = 'Please check your draft articles email address and try again.'
+      format.html{ render plain: content}
+      format.text{ render plain: content}
+    end
+  end
+end
