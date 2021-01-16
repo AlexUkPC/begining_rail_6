4c59448e11d3fa29b5a41bff3c8ef836815b423f
AlexUkPC <alexrogna@yahoo.com>
2021-01-16 14:06:09 +0000

    included ActiveModel and defined initialize method

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,13 @@
 class Car
-  
+  include ActiveModel::AttributeAssignment
   attr_accessor :make, :model, :year, :color
+
+  def initialize(attributes = {})
+    assign_attributes(attributes) if attributes
+
+    super()
+  end
+  
   def paint
     self.color = new_color
   end
