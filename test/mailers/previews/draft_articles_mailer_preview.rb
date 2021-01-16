26ef2e331bca6bc7ef27fa8f8c9b8243baeccd1c
AlexUkPC <alexrogna@yahoo.com>
2021-01-16 09:30:43 +0000

    responding to the author

--- test/mailers/previews/draft_articles_mailer_preview.rb
+++ test/mailers/previews/draft_articles_mailer_preview.rb
@@ -2,6 +2,10 @@
 class DraftArticlesMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/draft_articles_mailer/no_author
+  def created
+    DraftArticlesMailer.created('test@example.com', Article.first)
+  end
+  
   def no_author
     DraftArticlesMailer.no_author('test@example.com')
   end
