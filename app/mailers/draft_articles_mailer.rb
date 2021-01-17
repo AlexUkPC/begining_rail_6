26b26a7451070721f0a51d31494c1efbc026e648
AlexUkPC <alexrogna@yahoo.com>
2021-01-17 14:08:28 +0000

    typo

--- app/mailers/draft_articles_mailer.rb
+++ app/mailers/draft_articles_mailer.rb
@@ -10,7 +10,7 @@ class DraftArticlesMailer < ApplicationMailer
     mail to: to, subject: 'Your Draft Article has been created.'
   end
   
-  def no_author
+  def no_author(to)
     mail to: to, subject: "Your email could not be processed" do |format|
       content = 'Please check your draft articles email address and try again.'
       format.html{ render plain: content}
